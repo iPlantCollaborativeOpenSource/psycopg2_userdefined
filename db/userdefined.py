1f8f633f68fcb3e34f5d575868b63dbc2ea6d876
J. Matt Peterson <jmatt@jmatt.org>
2014-06-24 16:24:30 -0700

    Add get_type_name too, symmetry ftw. 	modified:   db/userdefined.py 	modified:   example.py

--- db/userdefined.py
+++ db/userdefined.py
@@ -6,7 +6,7 @@ import psycopg2
 
 def get_type_oid(cursor, typename, namespace="public"):
     """
-    Get a user defined type oid using a psycopg2 curson, the typename
+    Get a user defined type oid using a psycopg2 cursor, the typename
     and namespace.
     """
     cursor.execute("""
@@ -15,5 +15,19 @@ def get_type_oid(cursor, typename, namespace="public"):
               WHERE pgt.typname = %(typename)s
               AND   pgn.nspname = %(namespace)s;""",
                    {'typename': typename,
+                    'namespace': namespace})
+    return cursor.fetchone()[0]
+
+def get_type_name(cursor, oid, namespace="public"):
+    """
+    Get a user defined type name using a psycopg2 cursor, the oid
+    and namespace.
+    """
+    cursor.execute("""
+              SELECT pgt.oid FROM pg_type pgt
+              JOIN pg_namespace pgn ON pgt.typnamespace = pgn.oid
+              WHERE pgt.oid = %(oid)s
+              AND   pgn.nspname = %(namespace)s;""",
+                   {'oid': oid,
                     'namespace': namespace})
     return cursor.fetchone()[0]
--- example.py
+++ example.py
@@ -1,17 +1,18 @@
 """
-This is a specific example using DE's user-defined type acknowledgment_state.
+This is a specific trivial example using DE's user-defined type acknowledgment_state.
 To run in bash: python example.py
 """
 import psycopg2
 
-from db.userdefined import get_type_oid
+from db.userdefined import get_type_oid, get_type_name
 
 
 def main():
     conn = psycopg2.connect("host=host dbname=dbname "
                             "user=user password=password")
     a_state_oid = get_type_oid(conn.cursor(), "acknowledgment_state", "public")
-    print a_state_oid
+    a_state_name = get_type_oid(conn.cursor(), a_state_oid, "public")
+    print "%s = %s" % (a_state_name, a_state_oid)
 
 
 if __name__ == "__main__":
